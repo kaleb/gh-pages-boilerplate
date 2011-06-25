bdef4c859539276df1b94ca3506789a052ba7c06
Kaleb Hornsby <thekaleb@gmail.com>
2011-06-25 06:01:45 -0700

    Edited js/script.js via GitHub

--- js/script.js
+++ js/script.js
@@ -2,10 +2,8 @@
 
 */
 $(function(){
-  $('#site_header a').each(function(){
+  $('#site_header ul a').each(function(){
     var $this = $(this);
-    alert($this);
-    console.log($this);
     $this.button({
       icons: { primary: $this.attr('data-icon') }
     , text: false
